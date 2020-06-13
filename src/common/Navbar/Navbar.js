04d56dcbba2777c90d0ab053a38cb2490e88c68f
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-06-13 16:04:34 +0300

    added app navbar

---
+++ src/common/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => (
+    <nav className={ "navbar navbar-dark navbar-expand-lg bg-dark" }>
+        <div className="navbar-brand">Todo List</div>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/" exact>Home</NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
