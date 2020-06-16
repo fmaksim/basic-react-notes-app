742eb135502e3ac44b28e2517a30046ffcbf7958
Maksim Fomenok <maksim.fomenok@gmail.com>
2020-06-16 15:11:26 +0300

    wrap app component into alert context provider

--- src/app/app.js
+++ src/app/app.js
@@ -4,19 +4,24 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from "../pages/Home";
 import { About } from "../pages/About";
 import Navbar from "../common/Navbar";
+import { AlertState } from "../context/alert/AlertState";
+import Alert from "../common/Alert";
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Navbar />
-                <div className="container wrapper">
-                    <Switch>
-                        <Route path={ '/' } exact component={ Home }/>
-                        <Route path={ '/about' } component={ About }/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <AlertState>
+                <BrowserRouter>
+                    <Navbar/>
+                    <div className="container wrapper">
+                        <Alert />
+                        <Switch>
+                            <Route path={ '/' } exact component={ Home }/>
+                            <Route path={ '/about' } component={ About }/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </AlertState>
         );
     }
 }
